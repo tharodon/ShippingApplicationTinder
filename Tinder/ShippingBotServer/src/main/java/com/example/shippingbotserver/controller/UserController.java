921af00d7cb02a7a1b49fb7390e43f86dde1c0b1
tharodon <tharodon>
2022-04-12 16:26:14 +0300

    Controller rename

--- Tinder/ShippingBotServer/src/main/java/com/example/shippingbotserver/controller/UserController.java
+++ Tinder/ShippingBotServer/src/main/java/com/example/shippingbotserver/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
-public class ServerController {
+public class UserController {
 
     private final UserService userService;
 
